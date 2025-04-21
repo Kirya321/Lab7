c97269fc1e586970d5fc05bdf3684830ae064f87
Kirill Ketov <stud204043@vyatsu.ru>
2025-04-21 10:10:58 +0300

    Выполнил задание 14

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -2,6 +2,8 @@
 #include "XmlService.h"
 #include "JsonService.h"
 int main(){
+	JsonService js;
+	Weather W = js.getWeather("weather.json");
 	XmlService xs;
 	Weather w = xs.getWeather("weather.xml");
 	return 0;
